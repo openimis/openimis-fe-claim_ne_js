a3eac2adea481ede2a4720a7aad2ef10405c31dd
Purushottam Sapkota <puru601@gmail.com>
2020-03-31 11:36:37 +0545

    Added Attachemnt Filter in Claim Review

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import messages_en from "./translations/en.json";
 import ClaimFilterByInsureeGender from "./components/ClaimFilterByInsureeGender";
+import ClaimFilterHasAttachments from "./components/ClaimFilterHasAttachments";
 
 const DEFAULT_CONFIG = {
   "translations": [{ key: "en", messages: messages_en }],
-  "claim.Filter": [ClaimFilterByInsureeGender],
+  "claim.ReviewsFilter": [ClaimFilterByInsureeGender,ClaimFilterHasAttachments],
 }
 
 export const ClaimNeModule = (cfg) => {
